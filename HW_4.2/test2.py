f7661a9fb7faeae27a424c5a946199642ee36bc7
anton-zavalin <svertaketu@mail.ru>
2021-03-28 16:45:30 +0800

    HW_4.2

---
+++ HW_4.2/test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+  
+import os
+
+bash_command = ["cd ~/PycharmProjects/netology/devops-netology", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+
+for result in result_os.split('\n'):
+    if result.find('изменено') != -1:
+        prepare_result = ' ' +  bash_command[0].replace('cd ','')\
+                  + '/' + result.replace('\tизменено:      ', '')
+        print(prepare_result)
